2c02ab5cd7d386723925443bb00a651f0fe4c9b3
usr110 <m.aliabbas@gmail.com>
2018-03-28 15:51:23 +0100

    Add components to tabset and change labels of csv uploads

--- demo/csv_app.R
+++ demo/csv_app.R
@@ -2,25 +2,32 @@ library(shiny)
 
 source("../modules/csv_module.R")
 
-ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      csvFileUI("datafile_1", "User data (.csv format)"),
-      csvFileUI("datafile_2", "User data (.csv format)")
-    ),
-    mainPanel(
-      fluidRow(dataTableOutput("table_1")),
-      fluidRow(dataTableOutput("table_2"))
-    )
-  )
-)
+ui <- navbarPage("ITHIM",
+                 tabPanel("Introduction", 
+                          sidebarLayout(
+                            sidebarPanel(
+                              csvFileUI("datafile_1", "Travel Survey Data (.csv format)"),
+                              csvFileUI("datafile_2", "Health Survey Data (.csv format)")
+                            ),
+                            mainPanel(
+                              fluidRow(dataTableOutput("table_1")),
+                              fluidRow(dataTableOutput("table_2"))
+                            )
+                          )
+                 ),
+                 tabPanel("Component 1"),
+                 tabPanel("Component 2"),
+                 navbarMenu("More",
+                            tabPanel("Component 1"),
+                            tabPanel("Component 2")
+                 ))
 
 server <- function(input, output, session) {
   datafile_1 <- callModule(csvFile, "datafile_1",
-                         stringsAsFactors = FALSE)
+                           stringsAsFactors = FALSE)
   
   datafile_2 <- callModule(csvFile, "datafile_2",
-                         stringsAsFactors = FALSE)
+                           stringsAsFactors = FALSE)
   
   output$table_1 <- renderDataTable({
     datafile_1()
