af1f05d320fbd8a80145b381c3be529a5ecf5e0f
usr110 <m.aliabbas@gmail.com>
2018-03-29 15:50:08 +0100

    Clean code

--- demo/csv_app.R
+++ demo/csv_app.R
@@ -6,9 +6,9 @@ source("../modules/csv_module.R")
 ## Taken example from
 # https://github.com/AntoineGuillot2/Shiny_NextPreviousArrow
 
-Previous_Button=tags$div(actionButton("Prev_Tab",HTML('<div class="col-sm-4"><i class="fa fa-angle-double-left fa-2x"></i></div>
+Previous_Button <- tags$div(actionButton("Prev_Tab",HTML('<div class="col-sm-4"><i class="fa fa-angle-double-left fa-2x"></i></div>
                                                                   ')))
-Next_Button=div(actionButton("Next_Tab",HTML('<div class="col-sm-4"><i class="fa fa-angle-double-right fa-2x"></i></div>')))
+Next_Button <- div(actionButton("Next_Tab",HTML('<div class="col-sm-4"><i class="fa fa-angle-double-right fa-2x"></i></div>')))
 
 
 
@@ -101,31 +101,30 @@ server <- function(input, output, session) {
     updateTabsetPanel(session, "tabBox_next_previous", "Data Uploads")
   })
   
-  
-  
   output$Next_Previous=renderUI({
-    tab_list=input$List_of_tab[-length(input$List_of_tab)]
-    nb_tab=length(tab_list)
-    if (which(tab_list==input$tabBox_next_previous)==nb_tab)
-      column(1,offset=1,Previous_Button)
-    else if (which(tab_list==input$tabBox_next_previous)==1)
-      column(1,offset = 10,Next_Button)
+    tab_list <- input$List_of_tab[-length(input$List_of_tab)]
+    nb_tab <- length(tab_list)
+    if (which(tab_list == input$tabBox_next_previous) == nb_tab)
+      column(1, offset=1, Previous_Button)
+    else if (which(tab_list == input$tabBox_next_previous) == 1)
+      column(1, offset = 10, Next_Button)
     else
-      div(column(1,offset=1,Previous_Button),column(1,offset=8,Next_Button))
+      div(column(1, offset=1, Previous_Button), column(1, offset = 8, Next_Button))
     
   })
+  
   observeEvent(input$Prev_Tab,
                {
-                 tab_list=input$List_of_tab
-                 current_tab=which(tab_list==input$tabBox_next_previous)
-                 updateTabsetPanel(session,"tabBox_next_previous",selected=tab_list[current_tab-1])
+                 tab_list <- input$List_of_tab
+                 current_tab <- which(tab_list == input$tabBox_next_previous)
+                 updateTabsetPanel(session, "tabBox_next_previous",selected = tab_list[current_tab - 1])
                }
   )
   observeEvent(input$Next_Tab,
                {
-                 tab_list=input$List_of_tab
-                 current_tab=which(tab_list==input$tabBox_next_previous)
-                 updateTabsetPanel(session,"tabBox_next_previous",selected=tab_list[current_tab+1])
+                 tab_list <- input$List_of_tab
+                 current_tab <- which(tab_list == input$tabBox_next_previous)
+                 updateTabsetPanel(session, "tabBox_next_previous", selected=tab_list[current_tab + 1])
                }
   )
   
