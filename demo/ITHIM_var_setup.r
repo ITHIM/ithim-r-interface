5456506d5a903b5f98b7b414f4295479606d4b55
usr110 <m.aliabbas@gmail.com>
2018-04-10 12:04:57 +0100

    Remove miles cycled and car miles from summary tab

--- demo/ITHIM_var_setup.r
+++ demo/ITHIM_var_setup.r
@@ -8,10 +8,8 @@ regions <- generateRegionsList()
 # CO2
 
 variableRButton <- c("Number of Cyclists" =    "% Cyclists in the Total Population",
-                     "Miles Cycled" = "Total Miles Cycled Per Person (per week)",
                      "Physical Activity" = "Travel Marginal METs Per Person (per week)",
                      "Health" = "Years of Life Lost (YLL)",
-                     "Car Miles" = "Car Miles Per person (per week)",
                      "CO2" = "Transport CO2 Per Person (per week)"
 )
 carMilesRButton <- c("Car Miles" = "Car Miles Per person (per week)",
